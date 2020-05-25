1c49ab996b67207a8ac3f335f8c250f4cfa3b4b5
Juanma-8519 <juanma_8519@hotmail.com>
2020-05-25 10:40:35 +0200

    Revert "initial commit"
    This reverts commit c333759a143944ce5ef452b1b65a888792ace994.

--- XADC_UART_ASCII_TEMPERATURE.srcs/sources_1/new/fsm_states.vhd
+++
@@ -1,9 +0,0 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Package Declaration Section
-package fsm_states is
-
-    type Uart_Tx_State_type IS (IDLE, A, B, C, D, E);           -- Define the states
-
-end package fsm_states;
